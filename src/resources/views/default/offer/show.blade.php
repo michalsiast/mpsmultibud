409608ad0b9cdd32ab73c5204de82d5bfba1d802
Michał Siast <michalsiast@gmail.com>
2024-11-25 10:18:23 +0100

    Update show.blade.php

--- src/resources/views/default/offer/show.blade.php
+++ src/resources/views/default/offer/show.blade.php
@@ -12,10 +12,29 @@
                     <div class="text-wrapper">
                         {!! str_replace('<p>', '<p class="text">', $item->text) !!}
                     </div>
+                    <div class="gallery mt-5 mb-5">
+                        <div class="container">
+                            <div class="row">
+                                @foreach($item->gallery->items as $item)
+                                    @if($item->type !== 'cover')
+                                        <div class="col-lg-6 mt-2">
+                                            <a href="{{renderImage($item->url, 1920, 1080, 'resize')}}">
+                                                <img style="width: 100%" src="{{renderImage($item->url, 600, 600, 'fit')}}" alt="">
+                                            </a>
+                                        </div>
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 @include('default.offer.home3')
             </div>
         </div>
     </section>
-
+    @push('scripts.body.bottom')
+        <script>
+            var lightbox = $('.gallery a').simpleLightbox({});
+        </script>
+    @endpush
 @endsection
